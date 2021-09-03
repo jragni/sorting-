512015683de36ca516a85b361cc7ad7b04567b76
Ray Agni <jagni001@ucr.edu>
2021-09-03 12:34:02 -0700

    FEAT: completed merge helper function

--- merge.js
+++ merge.js
@@ -1,5 +1,35 @@
-function merge() {}
+/**Merge */
+
+function merge(arr1, arr2) {
+  let i = 0;
+  let j = 0;
+  let mergedArr = [];
+  let solution = [];
+  while (arr1[i] !== undefined && arr2[j] !== undefined) {
+    if (arr1[i] < arr2[j]) {
+      mergedArr.push(arr1[i]);
+      i++;
+    } else if (arr1[i] > arr2[j]) {
+      mergedArr.push(arr2[j]);
+      j++;
+    } else {
+      mergedArr.push(arr1[i]);
+      mergedArr.push(arr2[j]);
+      j++;
+      i++;
+    }
+  }
+
+  if (arr1[i] === undefined) {
+    return mergedArr.concat(arr2.slice(j));
+  } else if (arr2[j] === undefined) {
+    return mergedArr.concat(arr1.slice(i));
+  } else {
+    return mergedArr;
+  }
+}
 
 function mergeSort() {}
 
-module.exports = { merge, mergeSort};
+module.exports = { merge, mergeSort };
+
